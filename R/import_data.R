e30a21d30bb29c271bae986ba3e9a0bf98047418
MuysoKaos <esteban.gutierrez0501@gmail.com>
2026-06-11 11:18:47 -0500

    update model

--- R/import_data.R
+++ R/import_data.R
@@ -4,9 +4,7 @@ options(scipen = 1111)
 historial = readRDS("Data/Hist_Data.RDS")
 valores = readRDS("Data/Value_Data.RDS")
 
-datosmodel = crear_data_model(historial,valores) |>
-  dplyr::filter(diffatk < 400,
-                diffdef < 600)
+datosmodel = crear_data_model(historial,valores)
 
 datosmodel$goles1 = as.numeric(datosmodel$goles1)
 datosmodel$goles2 = as.numeric(datosmodel$goles2)
@@ -14,19 +12,24 @@ datosmodel$goles2 = as.numeric(datosmodel$goles2)
 resul = datosmodel |>
   dplyr::filter(equipo1 %in% historial$equipo1) |>
   dplyr::mutate(resul = ifelse(goles1>goles2,"Ganador",
-                               ifelse(goles2>goles1,"Losses","Draw")))
+                               ifelse(goles2>goles1,"Losses","Draw"))) |>
+  dplyr::left_join(valores, by = c("equipo2"="Equipo")) |>
+  dplyr::group_by(equipo1) |>
+  dplyr::mutate(encuentros = dplyr::n()) |>
+  dplyr::ungroup()
+
 
 avg_w = resul |>
   dplyr::filter(resul == "Ganador") |>
   dplyr::group_by(equipo1) |>
-  dplyr::summarise(atK_win = mean(diffatk),
-                   deff_win = mean(diffdef))
+  dplyr::summarise(atK_win = mean(ATK)*dplyr::n()/mean(encuentros),
+                   deff_win = mean(DEF)*dplyr::n()/mean(encuentros))
 
 avg_ls = resul |>
   dplyr::filter(resul == "Losses") |>
   dplyr::group_by(equipo1) |>
-  dplyr::summarise(atK_loss = mean(diffatk),
-                   deff_loss = mean(diffdef))
+  dplyr::summarise(atK_loss = mean(ATK)*dplyr::n()/mean(encuentros),
+                   deff_loss = mean(DEF)*dplyr::n()/mean(encuentros))
 
 datos_w = avg_w|>
   dplyr::left_join(avg_ls, by = "equipo1")|>
@@ -45,18 +48,26 @@ datos_std = datosmodel |>
                 atK_loss = (atK_loss-mean(datosmodel$atK_loss))/sd(datosmodel$atK_loss),
                 deff_loss = (deff_loss-mean(datosmodel$deff_loss))/sd(datosmodel$deff_loss))
 
-modelo_goles_hechos <- pscl::zeroinfl(goles1 ~   diffatk +atK_win |
-                                        diffdef+atK_win,
-                                      data = datos_std,
+datos_std_out = datos_std |>
+  dplyr::filter(diffatk <= 3,
+                diffdef <= 3,
+                atK_win<= 3,
+                deff_win<= 3,
+                atK_loss <= 3,
+                deff_loss <= 3)
+
+#diffatk+diffdef+atK_win+deff_win+atK_loss+deff_loss
+modelo_goles_hechos <- pscl::zeroinfl(goles1 ~   diffatk+diffdef+deff_win |
+                                        diffdef+deff_win,
+                                      data = datos_std_out,
                                       dist = "negbin", link = "probit")
 
-modelo_goles_recibidos <- pscl::zeroinfl(goles2 ~ diffatk + diffdef+atK_loss+deff_loss|
-                                           diffdef+atK_loss,
-                                         data = datos_std,
+#summary(modelo_goles_hechos)
+
+modelo_goles_recibidos <- pscl::zeroinfl(goles2 ~ diffdef+deff_win |
+                                           atK_win,
+                                         data = datos_std_out,
                                          dist = "negbin", link = "probit")
 
 
-
-
-#summary(modelo_goles_hechos)
 #summary(modelo_goles_recibidos)
